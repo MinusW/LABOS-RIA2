d29886d2c80fef1a9ffbe8b9a082ab745f93f58f
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2025-02-24 11:23:39 +0100

    feat: dom solution for Angular

--- virtual-dom-angular/src/app/app.component.ts
+++ virtual-dom-angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//TODO
+import {CounterComponent} from "./components/counter/counter.component";
+
 
 @Component({
   selector: 'app-root',
